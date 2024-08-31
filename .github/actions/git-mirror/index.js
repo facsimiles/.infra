2e8d6280cadc049f1730696f80237ed908575645
rindeal <dev.rindeal@gmail.com>
2024-08-31 05:32:27 +0200

    Update index.js

--- .github/actions/git-mirror/index.js
+++ .github/actions/git-mirror/index.js
@@ -66,10 +66,10 @@ function log(message, color = 'reset', emoji = '') {
     return `${colors.rgb(60, 60, 60)}0${colors.reset}`.repeat(len-str.length) + prefix + str + suffix
   }
   const elapsed = Date.now() - START_TIME
-  const seconds = formatNum(colors.rgb(100, 80, 220), Math.floor(elapsed / 1000), colors.reset, 3)
-  const milliseconds = formatNum(colors.rgb(80, 110, 250), elapsed % 1000, colors.reset, 3)
+  const seconds = formatNum(colors.rgb(110, 90, 220), Math.floor(elapsed / 1000), colors.reset, 3)
+  const milliseconds = formatNum(colors.rgb(80, 130, 255), elapsed % 1000, colors.reset, 3)
   
-  const elapsedStr = `${colors.rgb(170, 170, 170)}[${colors.reset}${colors.blue}+${colors.reset}${seconds}${colors.blue}.${colors.reset}${milliseconds}${colors.rgb(170, 170, 170)}ms]${colors.reset}`
+  const elapsedStr = `${colors.rgb(170, 170, 170)}[${colors.reset}${colors.blue}+${colors.reset}${seconds}${colors.blue}.${colors.reset}${milliseconds}${colors.rgb(130, 130, 130)}ms${colors.reset}${colors.rgb(170, 170, 170)}]${colors.reset}`
   
   console.log(`${elapsedStr} ${colors[color]}${emoji} ${message}${colors.reset}`)
 }
